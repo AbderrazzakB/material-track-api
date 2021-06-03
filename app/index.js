8eb33869776477a01ca9a775ee4a0030e1dbde52
chaimaa199 <chaimaaelhataf@gmail.com>
2021-06-03 15:09:18 +0100

    install

--- app/index.js
+++ app/index.js
@@ -4,6 +4,11 @@ dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import helmet from "helmet"
+import morgan from "morgan"
+import cors from "cors"
+
+
 
 // cookie-parser
 // helmet
@@ -22,6 +27,9 @@ import routes from "./routes/index.js";
 const app = express();
 const POST = 3005;
 app.use(cookieParser())
+app.use(helmet());
+app.use(morgan('combined'))
+app.use(cors())
 app.use(bodyParser.json());
 app.use(routes);
 
