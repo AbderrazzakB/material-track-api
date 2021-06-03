5e9820f10f66ab51ffab0f796b2065d464c31c83
Abdul Razzaq <abderrazzak.benbouya@gmail.com>
2021-06-03 16:09:43 +0100

    style: use prettier rules

--- app/routes/Material.js
+++ app/routes/Material.js
@@ -1,23 +1,23 @@
-import { Router } from "express";
-import Material from "../models/Material.js";
+import { Router } from 'express';
+import Material from '../models/Material.js';
 const router = Router();
 router
-  .get("/", (req, res) => {
-    res.json({ message: "Welcome in material route" });
+  .get('/', (req, res) => {
+    res.json({ message: 'Welcome in material route' });
   })
-  .get("/all", async (req, res) => {
+  .get('/all', async (req, res) => {
     try {
       const materials = await Material.find();
       res.json({ materials });
     } catch (error) {}
   })
-  .get("/:id", async (req, res) => {
+  .get('/:id', async (req, res) => {
     const id = req.params.id;
 
     try {
       const material = await Material.findById(id);
       if (!material) {
-        return res.json({ error: "not found" });
+        return res.json({ error: 'not found' });
       } else {
         res.json({ material });
       }
@@ -25,7 +25,7 @@ router
       res.json({ error });
     }
   })
-  .post("/", async (req, res) => {
+  .post('/', async (req, res) => {
     try {
       const { name, description, category } = req.body;
       const material = await Material.create({ name, description, category });
@@ -34,4 +34,4 @@ router
       res.json({ error });
     }
   });
-  export default router;
+export default router;
