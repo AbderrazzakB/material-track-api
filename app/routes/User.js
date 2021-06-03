95a7e378468b7dfa110bd230f56b9439897edd02
Abdul Razzaq <abderrazzak.benbouya@gmail.com>
2021-06-03 16:01:04 +0100

    style: use prettier rules

--- app/routes/User.js
+++ app/routes/User.js
@@ -1,24 +1,24 @@
-import { Router } from "express";
-import User from "../models/User.js";
+import { Router } from 'express';
+import User from '../models/User.js';
 const router = Router();
 
 router
-  .get("/", (req, res) => {
-    res.json({ message: "Welcome in user route" });
+  .get('/', (req, res) => {
+    res.json({ message: 'Welcome in user route' });
   })
-  .get("/all", async (req, res) => {
+  .get('/all', async (req, res) => {
     try {
       const users = await User.find();
       res.json({ users });
     } catch (error) {}
   })
-  .get("/:id", async (req, res) => {
+  .get('/:id', async (req, res) => {
     const id = req.params.id;
 
     try {
       const user = await User.findById(id);
       if (!user) {
-        return res.json({ error: "not found" });
+        return res.json({ error: 'not found' });
       } else {
         res.json({ user });
       }
@@ -26,7 +26,7 @@ router
       res.json({ error });
     }
   })
-  .post("/", async (req, res) => {
+  .post('/', async (req, res) => {
     try {
       const { fullName, email, password } = req.body;
       const user = await User.create({ fullName, email, password });
