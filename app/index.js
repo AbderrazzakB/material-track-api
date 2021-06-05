8ab1f50dd2ace335da0c7c2eb56533e5c4ee19a8
Abdul Razzaq <abderrazzak.benbouya@gmail.com>
2021-06-05 22:27:57 +0100

    style: use prettier style

--- app/index.js
+++ app/index.js
@@ -1,37 +1,36 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import helmet from "helmet"
-import morgan from "morgan"
-import cors from "cors"
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
 
-import db from "./utils/db.js";
+import db from './utils/db.js';
 db();
 
-import routes from "./routes/index.js";
+import routes from './routes/index.js';
 
 const app = express();
 const POST = 3005;
 
 // cookie-parser
-app.use(cookieParser())
+app.use(cookieParser());
 
 // helmet
 app.use(helmet());
 
 // morgan
-app.use(morgan('combined'))
+app.use(morgan('dev'));
 
 // cors
-app.use(cors())
+app.use(cors());
 
 // validation
 // argon2
 // jwt
-
 
 app.use(bodyParser.json());
 app.use(routes);
